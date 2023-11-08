cafb6465891bb8eb57fafcb926f936f285626d8c
maniksheikh <mdmanik081498@gmail.com>
2023-11-08 20:03:13 +0600

    created the home page

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express =require('express');
 const app =express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Manik');
-})
+    res.render('index.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
 
 app.listen(3000);
